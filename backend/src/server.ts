34f31e5015b4b61d3352b80844c2bd2eec0f4edc
parthmunjal07 <parth.munjal07@gmail.com>
2026-05-11 16:23:46 +0530

    Project init

---
+++ backend/src/server.ts
@@ -0,0 +1,21 @@
+import dotenv from 'dotenv'
+import express from "express"
+import 'dotenv/config';
+import { drizzle } from 'drizzle-orm/node-postgres';
+
+dotenv.config()
+
+const app = express()
+app.use(express.json())
+
+const port = process.env.PORT || 8079
+
+app.get('/', (req, res) => {
+    res.json({message: `Server is running on port: ${port}`})
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
+
+const db = drizzle(process.env.DATABASE_URL!);
